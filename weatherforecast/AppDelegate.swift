091be3f60e65b9a1415a36170e1d60c40402e842
Wahyu Sumartha <w.priya-dharma@mytaxi.com>
2020-01-01 13:01:13 +0100

    ^project setup with iOS 12 minimum deployment

---
+++ weatherforecast/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  weatherforecast
+//
+//  Created by Wahyu Sumartha Priya Dharma on 01.01.20.
+//  Copyright © 2020 Wahyu Sumartha Priya Dharma. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
